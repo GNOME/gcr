60bcb157893ac25069353fc18f0885bc6b55a2b7
Stef Walter <stefw@gnome.org>
2012-04-03 11:02:00 +0200

    Fix up GCR_DEBUG and GCK_DEBUG environment variables
     * This is due to the G_MESSAGES_DEBUG change in glib
     * We manage our own debug categories, so if G_MESSAGES_DEBUG
       is not being used but GCR_DEBUG or GCK_DEBUG are set
       we unconditionally print the debug messages.
     * If G_MESSAGES_DEBUG is being used, then make all the
       debug messages available to Glib.
     * Can build with -DGCR_DEBUG=category flags to permanently
       enable certain categories.

--- gck/gck-debug.c
+++ gck/gck-debug.c
@@ -58,10 +58,60 @@ _gck_debug_set_flags (const gchar *flags_string)
 }
 
 static void
+on_gck_log_debug (const gchar *log_domain,
+                  GLogLevelFlags log_level,
+                  const gchar *message,
+                  gpointer user_data)
+{
+	GString *gstring;
+	const gchar *progname;
+
+	gstring = g_string_new (NULL);
+
+	progname = g_get_prgname ();
+	g_string_append_printf (gstring, "(%s:%lu): %s-DEBUG: %s\n",
+	                        progname ? progname : "process",
+	                        (gulong)getpid (), log_domain,
+	                        message ? message : "(NULL) message");
+
+	write (1, gstring->str, gstring->len);
+	g_string_free (gstring, TRUE);
+}
+
+static void
 initialize_debug_flags (void)
 {
+	const gchar *messages_env;
+	const gchar *debug_env;
+
 	if (g_once_init_enter (&initialized_flags)) {
-		_gck_debug_set_flags (g_getenv ("GCK_DEBUG"));
+		messages_env = g_getenv ("G_MESSAGES_DEBUG");
+		debug_env = g_getenv ("GCK_DEBUG");
+#ifdef GCK_DEBUG
+		if (debug_env == NULL)
+			debug_env = G_STRINGIFY (GCK_DEBUG);
+#endif
+
+		/*
+		 * If the caller is selectively asking for certain debug
+		 * messages with the GCK_DEBUG environment variable, then
+		 * we install our own output handler and only print those
+		 * messages. This happens irrespective of G_MESSAGES_DEBUG
+		 */
+		if (messages_env == NULL && debug_env != NULL)
+			g_log_set_handler (G_LOG_DOMAIN, G_LOG_LEVEL_DEBUG,
+			                   on_gck_log_debug, NULL);
+
+		/*
+		 * If the caller is using G_MESSAGES_DEBUG then we enable
+		 * all our debug messages, and let Glib filter which ones
+		 * to display.
+		 */
+		if (messages_env != NULL && debug_env == NULL)
+			debug_env = "all";
+
+		_gck_debug_set_flags (debug_env);
+
 		g_once_init_leave (&initialized_flags, 1);
 	}
 }
@@ -79,20 +129,16 @@ _gck_debug_message (GckDebugFlags flag,
                     const gchar *format,
                     ...)
 {
-	gchar *message;
 	va_list args;
 
 	if G_UNLIKELY (!initialized_flags)
 		initialize_debug_flags ();
 
-	va_start (args, format);
-	message = g_strdup_vprintf (format, args);
-	va_end (args);
-
-	if (flag & current_flags)
-		g_log (G_LOG_DOMAIN, G_LOG_LEVEL_DEBUG, "%s", message);
-
-	g_free (message);
+	if (flag & current_flags) {
+		va_start (args, format);
+		g_logv (G_LOG_DOMAIN, G_LOG_LEVEL_DEBUG, format, args);
+		va_end (args);
+	}
 }
 
 #else /* !WITH_DEBUG */

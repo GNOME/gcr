3365b4404c1eb72efc54e37f4129d9657ff5aa5a
Corentin Noël <corentin.noel@collabora.com>
2021-12-09 11:30:30 +0100

    gcr: Remove deprecated methods
    Signed-off-by: Corentin Noël <corentin.noel@collabora.com>

--- gcr/gcr.h
+++ gcr/gcr.h
@@ -36,7 +36,6 @@
 #include <gcr/gcr-certificate-chain.h>
 #include <gcr/gcr-certificate-request.h>
 #include <gcr/gcr-column.h>
-#include <gcr/gcr-deprecated-base.h>
 #include <gcr/gcr-enum-types.h>
 #include <gcr/gcr-fingerprint.h>
 #include <gcr/gcr-icons.h>

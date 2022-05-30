9fa123ed94ba9bff5b1890e3922bcf374ca7c476
Corentin Noël <corentin.noel@collabora.com>
2022-05-30 23:52:41 +0200

    secure-memory: Fix odr-violation by using a private setter
    (cherry picked from commit b6a26c64394f924c2766f57a9218213ad9bf1d70)

---
+++ egg/egg-secure-memory-private.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2022 Collabora Ltd. (https://collabora.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Corentin Noël <corentin.noel@collabora.com>
+ */
+
+#ifndef EGG_SECURE_MEMORY_PRIVATE_H
+#define EGG_SECURE_MEMORY_PRIVATE_H
+
+#include <stdlib.h>
+
+void egg_set_secure_warnings (int val);
+
+#endif /* EGG_SECURE_MEMORY_PRIVATE_H */

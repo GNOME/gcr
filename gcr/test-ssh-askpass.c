056d16fa27350066af51ed98803c87291d7200ef
Niels De Graef <nielsdegraef@gmail.com>
2023-01-22 19:27:26 +0100

    test-ssh-askpass: Use g_assert_no_errno()
    If `g_chmod()` fails for some reason, it's better to know why it's failing

--- gcr/test-ssh-askpass.c
+++ gcr/test-ssh-askpass.c
@@ -77,8 +77,7 @@ test_ssh_keygen (Test *test,
 
 	g_assert_cmpstr (password, ==, "booo");
 
-	if (g_chmod (filename, 0600) < 0)
-		g_assert_not_reached ();
+	g_assert_no_errno (g_chmod (filename, 0600));
 
 	g_spawn_async (SRCDIR "/gcr/fixtures", argv, NULL,
 	               G_SPAWN_STDOUT_TO_DEV_NULL | G_SPAWN_SEARCH_PATH | G_SPAWN_DO_NOT_REAP_CHILD | G_SPAWN_CLOEXEC_PIPES,

10ad3274f1d58242e55afa6e09cefc93dbf0d130
Dhanuka Warusadura <dhanuka@gnome.org>
2024-03-01 21:08:51 +0530

    ssh-agent: avoid deadlock when agent process dies before we connect to it
    gkd_ssh_agent_process_connect() waits for the ssh-agent process to
    become ready to accept input by entering the main loop while holding
    self->lock.  However, if the ssh-agent process dies before becoming
    ready, the main loop will call on_child_watch(), which needs to take
    self->lock, causing a deadlock.  Fix this by releasing the lock before
    entering the main loop.
    
    This should prevent a busyloop that's been reported multiple times [1]
    [2] from lasting forever.
    
    [1] https://bugzilla.gnome.org/show_bug.cgi?id=794848
    [2] https://bugzilla.redhat.com/show_bug.cgi?id=1841855 (migrated to
    https://issues.redhat.com/browse/RHEL-9302)
    
    Original author: Steven Luo

--- gcr/gcr-ssh-agent-process.c
+++ gcr/gcr-ssh-agent-process.c
@@ -261,8 +261,11 @@ gcr_ssh_agent_process_connect (GcrSshAgentProcess *self,
 
 	if (started && !self->ready) {
 		source = g_timeout_add_seconds (5, on_timeout, &timedout);
-		while (!self->ready && !timedout)
+		while (!self->ready && !timedout) {
+                        g_mutex_unlock (&self->lock);
 			g_main_context_iteration (NULL, FALSE);
+                        g_mutex_lock (&self->lock);
+                }
 		g_source_remove (source);
 	}
 
